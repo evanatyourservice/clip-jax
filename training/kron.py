ccde172e7ab8cae347ce264271d18919fdc3c0ee
Evan Walters <evanatyourservice@gmail.com>
2024-10-20 19:40:11 -0600

    Update kron.py

--- training/kron.py
+++ training/kron.py
@@ -49,8 +49,8 @@ def scale_by_kron(
     min_ndim_triangular: int = 2,
     mu_dtype: Optional[Union[str, jnp.dtype]] = None,
     precond_dtype: Optional[Union[str, jnp.dtype]] = None,
-    precond_update_precision: str = "float32",
-    precond_grads_precision: str = "bfloat16",
+    precond_update_precision: str = "tensorfloat32",
+    precond_grads_precision: str = "tensorfloat32",
     scanned_layers: Optional[base.Params] = None,
     lax_map_scanned_layers: bool = False,
     lax_map_batch_size: int = 8,
@@ -295,24 +295,11 @@ def scale_by_kron(
                 )
             ]
 
-        # print metrics
-        jax.lax.cond(
-            count_inc % 25 == 0,
-            lambda: jax.debug.print(
-                "abs(x) {abs:.8e} x^2 {x2:.8e} x^4 {x4:.8e} max {max:.8e}",
-                abs=jnp.array([jnp.mean(jnp.abs(x)) for x in precond_gs]).mean(),
-                x2=jnp.array([jnp.mean(jnp.abs(x) ** 2) for x in precond_gs]).mean(),
-                x4=jnp.array([jnp.mean(jnp.abs(x) ** 4) for x in precond_gs]).mean(),
-                max=jnp.array([jnp.max(jnp.abs(x)) for x in precond_gs]).max(),
-            ),
-            lambda: None,
-        )
-
         # trust region
         # precond_gs = jax.tree.map(
         #     lambda x: jnp.sign(x) * jnp.log(jnp.abs(x) + 1), precond_gs
         # )
-        precond_gs = jax.tree.map(lambda x: jnp.tanh(x / 2) * 2, precond_gs)
+        precond_gs = jax.tree.map(jnp.tanh, precond_gs)
 
         # box preconditioned grads
         if flax_partitioned:
@@ -347,8 +334,8 @@ def kron(
     min_ndim_triangular: int = 2,
     mu_dtype: Optional[Union[str, jnp.dtype]] = None,
     precond_dtype: Optional[Union[str, jnp.dtype]] = None,
-    precond_update_precision: str = "float32",
-    precond_grads_precision: str = "bfloat16",
+    precond_update_precision: str = "tensorfloat32",
+    precond_grads_precision: str = "tensorfloat32",
     scanned_layers: Optional[base.Params] = None,
     lax_map_scanned_layers: bool = False,
     lax_map_batch_size: int = 8,
